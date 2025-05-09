5ebb4afddcdc531a4006ffe8a967accc429f34a5
PrithwisK07 <karmakarprithwis566@gmail.com>
2025-05-09 12:08:21 +0530

    comments added

--- server/server.js
+++ server/server.js
@@ -7,9 +7,9 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 
-
 const PORT = process.env.PORT || 5500;
 
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
@@ -19,14 +19,20 @@ mongoose
     console.log(e);
   });
 
-  app.use(
-    cors({
-      origin: 'http://localhost:5173',  
-      methods: ['GET', 'POST', 'DELETE', 'PUT'],
-      allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control', 'Expires', 'Pragma'],
-      credentials: true,
-    })  
-  );
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
 
 app.use("/api", router);
 
@@ -34,4 +40,4 @@ app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
 });
 
-module.exports=app;
+module.exports = app;
