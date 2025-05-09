ae2950487059f9c1bd63aa341831d85510d2886f
Arnab Paul <159263669+Arnabpaul0101@users.noreply.github.com>
2025-05-09 12:32:27 +0530

    Revert "Added a comment to login"

--- server/models/User.js
+++ server/models/User.js
@@ -10,7 +10,7 @@ const noteSchema = new mongoose.Schema({
         required: true
     }
 });
-// user schmema comment
+
 const userschema=new mongoose.Schema({
     usercode:{
         type:String,
